42915ae3b7f274943a2425bad64a125cba3738ad
rajangirsa <rajan15161@iiitd.ac.in>
2024-04-09 19:30:00 +0530

    Added endpoint to download gtfs.

--- blueprints/pmpml/apis.py
+++ blueprints/pmpml/apis.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, send_file
 
 from utils.main import get_routes_func, get_stops_func, get_transit_route_details_func, get_routes_on_stop_func, \
     make_combined_response
@@ -28,25 +28,25 @@ def require_api_key(api_key):
     return decorator
 
 
-@pmpml_bp.route('/get_routes', endpoint='get_routes_api')
+@pmpml_bp.route('/routes', endpoint='get_routes_api')
 @require_api_key(API_KEY)
 def get_routes_api():
     return get_routes_func()
 
 
-@pmpml_bp.route('/get_complete_info', endpoint='get_complete_routes_info')
+@pmpml_bp.route('/complete_info', endpoint='get_complete_routes_info')
 @require_api_key(API_KEY)
 def get_routes_api():
     return make_combined_response()
 
 
-@pmpml_bp.route('/get_stops', endpoint='get_stops_api')
+@pmpml_bp.route('/stops', endpoint='get_stops_api')
 @require_api_key(API_KEY)
 def get_stops_api():
     return get_stops_func()
 
 
-@pmpml_bp.route('/get_transit_route_details', endpoint='get_transit_route_details')
+@pmpml_bp.route('/transit_route_details', endpoint='get_transit_route_details')
 @require_api_key(API_KEY)
 def get_transit_route_details():
     data = None
@@ -58,7 +58,7 @@ def get_transit_route_details():
         return 'Wrong route', 400
 
 
-@pmpml_bp.route('/get_routes_on_stop', endpoint='get_routes_on_stop')
+@pmpml_bp.route('/routes_on_stop', endpoint='get_routes_on_stop')
 @require_api_key(API_KEY)
 def get_routes_on_stop():
     data = None
@@ -68,3 +68,15 @@ def get_routes_on_stop():
         return get_routes_on_stop_func(int(data['stop_id']), data['time'] if 'time' in data.keys() else None)
     else:
         return 'Wrong route', 400
+
+
+@pmpml_bp.route('/gtfs_zip', endpoint='get_gtfs_zip')
+def get_gtfs_zip():
+    # To zip use
+    # zip -9 pmpml_gtfs_.zip routes.txt shapes.txt stops.txt stop_times.txt trips.txt
+    path_to_zip = 'static/data/GTFS/gtfs.zip'
+
+    if not os.path.exists(path_to_zip):
+        return "File not found.", 404
+
+    return send_file(path_to_zip, as_attachment=True, download_name='pmpml_gtfs.zip')
